1646191b913367cfb5f9b26a3dcadb22b50e41fd
artemRa <aaartem.ra@gmail.com>
2023-04-29 23:53:36 +0400

    YAML testing

--- eng_auto_dict.R
+++ eng_auto_dict.R
@@ -7,3 +7,8 @@ library(glue)
 library(reticulate)
 library(tidytext)
 library(praise)
+
+
+yaml::write_yaml(list(a = list(1, 2), b = "X"), "config.yaml")
+config <- read_yaml("config.yaml")
+config[["a"]]
